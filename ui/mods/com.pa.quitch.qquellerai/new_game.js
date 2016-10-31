7440d8c98e215b89a0d466462fce6d82bcceb92f
Quitch <quitch@users.noreply.github.com>
2016-10-31 13:51:10 +0000

    Standardise Uber bot advanced eco check with other levels

--- ui/mods/com.pa.quitch.qquellerai/new_game.js
+++ ui/mods/com.pa.quitch.qquellerai/new_game.js
@@ -235,7 +235,7 @@ function qQuellerAI() {
                 priority_scout_metal_spots: true,
                 enable_commander_danger_responses: true,
                 neural_data_mod: 0.5,
-                adv_eco_mod: 1.4,
+                adv_eco_mod: 1.3,
                 adv_eco_mod_alone: 0.85,
                 personality_tags:
                 [
