9d5e6df88340c2b4d79189ccd4fa8ff81373ea15
Quitch <quitch@users.noreply.github.com>
2016-11-20 23:23:58 +0000

    Fix Uber Cautious tech modifier

--- ui/mods/com.pa.quitch.qquellerai/new_game.js
+++ ui/mods/com.pa.quitch.qquellerai/new_game.js
@@ -234,7 +234,7 @@ function qQuellerAI() {
                 priority_scout_metal_spots: true,
                 enable_commander_danger_responses: true,
                 neural_data_mod: 0.5,
-                adv_eco_mod: 1.3,
+                adv_eco_mod: 1.0,
                 adv_eco_mod_alone: 0.85,
                 personality_tags:
                 [
