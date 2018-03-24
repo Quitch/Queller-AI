24e87c0d33a520f0a626cf18e7cc6aba8090884c
Quitch <Quitch@users.noreply.github.com>
2018-03-24 01:16:01 +0000

    Gold techs earlier

--- ui/mods/com.pa.quitch.qquellerai/new_game.js
+++ ui/mods/com.pa.quitch.qquellerai/new_game.js
@@ -88,8 +88,8 @@ function qQuellerAI() {
             priority_scout_metal_spots: true,
             enable_commander_danger_responses: true,
             neural_data_mod: 1.3,
-            adv_eco_mod: 1.0,
-            adv_eco_mod_alone: 0.85,
+            adv_eco_mod: 0.75,
+            adv_eco_mod_alone: 0.75,
             personality_tags:
                 [
                     "gold"
