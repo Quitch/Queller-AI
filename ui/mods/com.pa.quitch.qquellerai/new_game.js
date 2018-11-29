ad3f742c8f38fd4ab04fa8f0193b90ac87cdb672
Quitch <Quitch@users.noreply.github.com>
2018-11-29 10:53:36 +0000

    Version it

--- ui/mods/com.pa.quitch.qquellerai/new_game.js
+++ ui/mods/com.pa.quitch.qquellerai/new_game.js
@@ -351,7 +351,7 @@ function qQuellerAI() {
     model.aiPersonalities.valueHasMutated();
 
     _.defer(function () {
-        model.localChatMessage(loc("!LOC:Queller AI"), loc("!LOC:by Quitch. Version #.##.#. My difficulties are prefixed with a Q.  Don't make AI teams larger than human teams, increase the difficulty or the econ rate instead. I support the Legion Expansion."));
+        model.localChatMessage(loc("!LOC:Queller AI"), loc("!LOC:by Quitch. Version 4.38.0. My difficulties are prefixed with a Q.  Don't make AI teams larger than human teams, increase the difficulty or the econ rate instead. I support the Legion Expansion."));
     });
 
 }
