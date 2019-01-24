74887e4eef6d00f5d5156066f84d61e007c0a40a
Quitch <Quitch@users.noreply.github.com>
2019-01-24 13:27:26 +0000

    Move naval eco changes out of test

--- ui/mods/com.pa.quitch.qquellerai/new_game.js
+++ ui/mods/com.pa.quitch.qquellerai/new_game.js
@@ -223,27 +223,6 @@ function qQuellerAI() {
     },
     'Q-Uber Naval': {
       display_name: '!LOC:Q-Uber Naval',
-      metal_drain_check: 0.54,
-      energy_drain_check: 0.65,
-      metal_demand_check: 0.85,
-      energy_demand_check: 0.8,
-      micro_type: 2,
-      go_for_the_kill: true,
-      priority_scout_metal_spots: true,
-      enable_commander_danger_responses: true,
-      neural_data_mod: 1.0,
-      adv_eco_mod: 1.0,
-      adv_eco_mod_alone: 0,
-      personality_tags:
-        [
-          "uber",
-          "naval"
-        ],
-      min_basic_fabbers: 1,
-      min_advanced_fabbers: 1
-    },
-    'Q-Uber Test Naval': {
-      display_name: '!LOC:Q-Uber Test Naval',
       metal_drain_check: 0.64,
       energy_drain_check: 0.65,
       metal_demand_check: 0.85,
