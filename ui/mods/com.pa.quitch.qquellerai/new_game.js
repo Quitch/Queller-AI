37e88d45af54885233a7d9c1121e5d76109bb640
Quitch <Quitch@users.noreply.github.com>
2022-01-31 22:31:25 +0000

    Clarify comment

--- ui/mods/com.pa.quitch.qquellerai/new_game.js
+++ ui/mods/com.pa.quitch.qquellerai/new_game.js
@@ -256,7 +256,7 @@ if (!quellerAILoaded) {
       _.assign(model.aiPersonalities(), newPersonalities);
       model.aiPersonalities.valueHasMutated();
 
-      // assign random personalities when start game clicked
+      // assign personalities to Randoms when start game clicked
       model.startGame = (function () {
         var cachedFunction = model.startGame;
 
